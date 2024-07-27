917dd28b6210064ba097e5b94e220fb3cac35512
Kabuto43 <worldofwarcrafttop@gmail.com>
2024-07-28 02:12:27 +0300

    general Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import { Navigation } from "./Navigation"
 import { QRCodeGenerator } from './QRCodeGenerator'
 import { QRCodeScanner} from './QRCodeScanner'
 
 export const Layout = () => {
 	return (
-		<>
-			{/* <QRCodeGenerator/> */}
-			<QRCodeScanner/>
-		</>
+		<div>
+			<Navigation/>
+
+			<Routes>
+        <Route path="/generate" element={<QRCodeGenerator />}/>
+        <Route path="/scan" element={<QRCodeScanner />}/>
+      </Routes>
+		</div>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
-// import { Layout } from './Layout'
-import { QRCodeGenerator } from './QRCodeGenerator'
-import { QRCodeScanner} from './QRCodeScanner'
-import { Navigation } from "./Navigation"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigation/>,
-  },
-  {
-    path: "/generate",
-    element: <QRCodeGenerator/>,
-  },
-  {
-    path: "/scan",
-    element: <QRCodeScanner/>,
-  }
-])
+import { BrowserRouter } from "react-router-dom"
+import { Layout } from './Layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>,
+  <BrowserRouter>
+    <Layout/>
+  </BrowserRouter>
 )
